a528ecae1a7b135668febb7d3a744647f3de8b39
Marcos Martini <marcosrmartini@gmail.com>
2017-06-13 06:46:45 -0300

    Adição de cadastros

--- src/main/java/br/com/mmartini/gestao/controller/FabricantesController.java
+++ src/main/java/br/com/mmartini/gestao/controller/FabricantesController.java
@@ -1,18 +1,35 @@
 package br.com.mmartini.gestao.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.com.mmartini.gestao.model.Convidado;
+import br.com.mmartini.gestao.model.Fabricante;
+import br.com.mmartini.gestao.repository.FabricanteRepository;
 
 @Controller
 @RequestMapping("/cadastro/fabricante")
 public class FabricantesController {
 
+	@Autowired
+	private FabricanteRepository fabricantes;
+	
 	@GetMapping
 	public ModelAndView listar(){
 		ModelAndView mv = new ModelAndView("cadastro/Fabricante");
+		mv.addObject("fabricantes", fabricantes.findAll());
+		mv.addObject(new Fabricante());
 		return mv;
+	}
+	
+	@PostMapping
+	public String salvar(Fabricante fabricante){
+		fabricantes.save(fabricante);
+		return "redirect:/cadastro/fabricante";
 	}
 	
 }
--- src/main/java/br/com/mmartini/gestao/model/Fabricante.java
+++ src/main/java/br/com/mmartini/gestao/model/Fabricante.java
@@ -1,12 +1,15 @@
 package br.com.mmartini.gestao.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Fabricante {
+public class Fabricante implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue
 	private Long idFabricante;
