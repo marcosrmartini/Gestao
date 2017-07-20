514756dea8bac5fd5baecaea07190f139c3dd79c
Marcos Martini <marcosrmartini@gmail.com>
2017-07-20 07:44:45 -0300

    Correção de pesquisa de produto

--- src/main/java/br/com/mmartini/gestao/controller/ProdutoController.java
+++ src/main/java/br/com/mmartini/gestao/controller/ProdutoController.java
@@ -1,10 +1,9 @@
 package br.com.mmartini.gestao.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,10 +45,9 @@ public class ProdutoController {
 	}
 
 	@PostMapping("pesquisaProduto")
-	private ModelAndView pesquisaProduto(HttpServletRequest request){
+	private ModelAndView pesquisaProduto(@ModelAttribute("produto") Produto produto){
 		ModelAndView mv = new ModelAndView("/cadastro/PesquisaProduto");
-		Produto p = (Produto) request.getSession().getAttribute("produto");
-		mv.addObject("produtos", produtos.pesquisaPorDescricao("%" + p.getDescricao().toUpperCase() + "%"));
+		mv.addObject("produtos", produtos.pesquisaPorDescricao("%" + produto.getDescricao().toUpperCase() + "%"));
 		return mv;
 	}
 	
