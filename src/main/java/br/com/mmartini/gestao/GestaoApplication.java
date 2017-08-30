5ed24ea6b9d139e7b9a21c3dc92ba349ad762317
Marcos Martini <marcosrmartini@gmail.com>
2017-08-30 07:37:05 -0300

    Configurado suporte a jsf

--- src/main/java/br/com/mmartini/gestao/GestaoApplication.java
+++ src/main/java/br/com/mmartini/gestao/GestaoApplication.java
@@ -1,7 +1,15 @@
-	package br.com.mmartini.gestao;
+package br.com.mmartini.gestao;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.sun.faces.config.ConfigureListener;
 
 @SpringBootApplication
 public class GestaoApplication {
@@ -9,4 +17,32 @@ public class GestaoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoApplication.class, args);
 	}
+
+//	@Bean
+//	public ServletRegistrationBean servletRegistrationBean() {
+//		FacesServlet servlet = new FacesServlet();
+//		ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(
+//				servlet, "*.jsf");
+//		return servletRegistrationBean;
+//	}
+	
+
+    @Bean
+    public ServletRegistrationBean facesServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+            new FacesServlet(), "*.jsf");
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+        return new ServletListenerRegistrationBean<ConfigureListener>(
+            new ConfigureListener());
+    }
+
+    
+    public void setServletContext(ServletContext servletContext) {
+        servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());       
+    }
 }
